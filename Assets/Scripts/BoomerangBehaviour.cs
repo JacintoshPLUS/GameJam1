2972cc03227ee3525e48670d0f179685b4df4a2e
Toastmalone28 <hohmannjulius1@gmail.com>
2024-09-17 12:32:20 +0100

    Boomerang update

--- Assets/Scripts/BoomerangBehaviour.cs
+++ Assets/Scripts/BoomerangBehaviour.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BoomerangBehaviour : MonoBehaviour
 {
     public BoomerangState state;
-    public Rigidbody rb;
-    public Vector3 StartPosition;
+    private Rigidbody rb;
+    private Vector3 StartPosition;
+    public int speed;
+    public int range;
+    public int rotationSpeed;
     void Start()
     {
         state = BoomerangState.Ready;
@@ -24,13 +28,39 @@ public class BoomerangBehaviour : MonoBehaviour
         if (state == BoomerangState.Moving)
         {
             StopBoomerang();
+            Spin();
         }
+        if (state == BoomerangState.ReachedEnd)
+        {
+            StartCoroutine(ReturnBoomerang());
+            Spin();
+        }
+    }
+
+    private void MoveBoomerangBack()
+    {
+    }
+
+    private IEnumerator ReturnBoomerang()
+    {
+        yield return new WaitForSeconds(1f);
+        state = BoomerangState.Returning;
+        transform.position = Vector3.MoveTowards(transform.position, StartPosition, 10f);
+        state = BoomerangState.Ready;
+    }
+
+    private void Spin()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 
     private void StopBoomerang()
     {
-        if (transform.position.z >= StartPosition.z + 10)
+        if (transform.position.z >= StartPosition.z + range)
+        {
             rb.velocity = Vector3.zero;
+            state = BoomerangState.ReachedEnd;
+        }
     }
 
     public void MoveBoomerang()
@@ -38,6 +68,6 @@ public class BoomerangBehaviour : MonoBehaviour
         Debug.Log("Boomerang thrown");
         StartPosition = transform.position;
         state = BoomerangState.Moving;
-        rb.AddForce(new Vector3(0, 0, 5), ForceMode.Impulse);
+        rb.AddForce(new Vector3(0, 0, speed), ForceMode.Impulse);
     }
 }
